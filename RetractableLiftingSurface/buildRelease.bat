ac90496ff382a68c3123828e6e651554004470e9
Jonathan Bayer <linuxgurugamer@gmail.com>
2017-05-28 16:36:33 -0400

    Updated for 1.3

--- RetractableLiftingSurface/buildRelease.bat
+++ RetractableLiftingSurface/buildRelease.bat
@@ -1,24 +1,7 @@
 ﻿@echo off
-set DEFHOMEDRIVE=d:
-set DEFHOMEDIR=%DEFHOMEDRIVE%%HOMEPATH%
-set HOMEDIR=
-set HOMEDRIVE=%CD:~0,2%
 
 set RELEASEDIR=d:\Users\jbb\release
 set ZIP="c:\Program Files\7-zip\7z.exe"
-echo Default homedir: %DEFHOMEDIR%
-
-rem set /p HOMEDIR= "Enter Home directory, or <CR> for default: "
-
-if "%HOMEDIR%" == "" (
-set HOMEDIR=%DEFHOMEDIR%
-)
-echo %HOMEDIR%
-
-SET _test=%HOMEDIR:~1,1%
-if "%_test%" == ":" (
-set HOMEDRIVE=%HOMEDIR:~0,2%
-)
 
 
 set VERSIONFILE=RetractableLiftingSurface.version
@@ -41,27 +24,13 @@ if "%build%" NEQ "0"  set VERSION=%VERSION%.%build%
 echo Version:  %VERSION%
 
 
-
-set d=%HOMEDIR\install
-if exist %d% goto one
-mkdir %d%
-:one
-set d=%HOMEDIR%\install\Gamedata
-if exist %d% goto two
-mkdir %d%
-:two
-
-rmdir /s /q %HOMEDIR%\install\Gamedata\RetractableLiftingSurface
-
 copy bin\Release\RetractableLiftingSurface.dll ..\GameData\RetractableLiftingSurface\Plugins
 copy  RetractableLiftingSurface.version ..\GameData\RetractableLiftingSurface\RetractableLiftingSurface.version
 
-xcopy /Y /E ..\GameData\RetractableLiftingSurface  %HOMEDIR%\install\Gamedata\RetractableLiftingSurface\
-copy /y ..\LICENSE %HOMEDIR%\install\Gamedata\RetractableLiftingSurface
+copy /y ..\LICENSE ..\Gamedata\RetractableLiftingSurface
 
-%HOMEDRIVE%
-cd %HOMEDIR%\install
-
+cd ..
 set FILE="%RELEASEDIR%\RetractableLiftingSurface-%VERSION%.zip"
 IF EXIST %FILE% del /F %FILE%
 %ZIP% a -tzip %FILE% GameData\RetractableLiftingSurface 
+pause
